55d00bf26fbb917a21f17880e2b0933e7954145c
IVO SINGER <47070407@ort>
2023-03-15 09:01:25 -0300

    arreglar errores y probar

--- Pizzas.api/Controllers/PizzasController.cs
+++ Pizzas.api/Controllers/PizzasController.cs
@@ -1,29 +1,74 @@
 namespace Pizzas.api.Controllers
 {
-using Microsoft.AspNetCore.Mvc;
-using Pizzas.api.Models;
-    [ApiController]
-    [Route("[controller]")]
-    public class PizzasController : ControllerBase
-    {
-        [HttpGet]
-        public IActionResult GetAll(){
-            List<Pizzas> lista = BD.ObtenerPizzas();
-            return Ok(lista);
+        using Microsoft.AspNetCore.Mvc;
+        using Pizzas.api.Models;
+        [ApiController]
+        [Route("[controller]")]
+        public class PizzasController : ControllerBase
+        {
+            [HttpGet]
+            public IActionResult GetAll(){
+                List<Pizzas> lista = BD.ObtenerPizzas();
+                return Ok(lista);
+            }
+
+
+            [HttpGet("(id)")]
+        public IActionResult Get(int id){
+            if(id<1){
+                return BadRequest();
+            }
+            Pizzas p = BD.ObtenerP(id);
+            if(p==null)
+            {
+                return NotFound();
+            }
+            return Ok(p);
+        }
+            
+            [HttpPost]
+        public IActionResult Create(Pizzas pizza){
+            BD.CrearPizza(pizza);
+            return Ok();
         }
 
 
-         [HttpGet("(id)")]
-    public IActionResult Get(int id){
-        if(id<1){
+        [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizzas pizza)
+    {
+
+    if(id < 1){
             return BadRequest();
         }
         Pizzas p = BD.ObtenerP(id);
-        if(p==null)
-        {
+
+        if(p == null) {
             return NotFound();
         }
-        return Ok(p);
+
+        BD.UpdatePizza(id, pizza);
+        return Ok();
+
     }
+
+
+        [HttpDelete("{id}")]
+    public IActionResult Delete(int id){
+        if(id < 1){
+            return BadRequest();
+        }
+        Pizzas p = BD.ObtenerP(id);
+
+        if(p == null) {
+            return NotFound();
+        }
+
+        BD.EliminarPizza(id);
+        return Ok();
+    }
+
+
+
+
     }
 }
--- Pizzas.api/Models/BD.cs
+++ Pizzas.api/Models/BD.cs
@@ -7,7 +7,7 @@ namespace Pizzas.api.Models
 
 
     public class BD{
-        private static string _connectionString = @"Server=A-PHZ2-CIDI-007;DataBase=DAI-Pizzas;Trusted_Connection=True";
+        private static string _connectionString = @"Server=A-PHZ2-AMI-005;DataBase=DAI-Pizzas;Trusted_Connection=True";
         public static List<Pizzas> ObtenerPizzas()
         {
             List<Pizzas> lista = new List<Pizzas>();
@@ -28,6 +28,29 @@ namespace Pizzas.api.Models
             }
             return pizza;
         }
+        public static List<Pizzas> CrearPizza(Pizzas p){
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            connection.Execute("INSERT INTO Pizzas VALUES (@pNombre, @pLibreGluten, @pImporte, @pDescripcion)", new { pNombre = p.Nombre, pLibreGluten = p.LibreGluten, pImporte = p.Importe, pDescripcion = p.Descripcion});
+        }
+        
+        }
+         public static void UpdatePizza(int id, Pizzas p)
+    {
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            connection.Execute("UPDATE Pizzas SET Nombre = @pNombre, LibreGluten = @pLibreGluten, Importe = @pImporte, Descripciom = @pDescripcion WHERE Id = @pId", new { pNombre = p.Nombre, pLibreGluten = p.LibreGluten, pImporte = p.Importe, pDescripcion = p.Descripcion});
+        }
+    }
+        
+         public static void EliminarPizza(int id)
+    {
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            connection.Execute("DELETE FROM Pizzas WHERE Id = @pId", new { pId = id });
+        }
+    }
+        
     }
 
 }
